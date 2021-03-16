c9808526ebb02a2e4b7f3f8f94287ef152254ba1
gandhiboys <manasgandhi2@gmail.com>
2021-03-16 11:39:20 +0530

    minor changes

--- test.java
+++ test.java
@@ -4,5 +4,6 @@ class Test {
     public static void main(String[] args) {
         System.out.println("Test file");
         System.out.println("Hello Jenkins"); 
+        System.out.println("Hello Jenkins 1"); 
     }
 }
